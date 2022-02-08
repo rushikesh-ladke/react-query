b169d05251e08d0f1f6c6d933617c5a158488773
rushi-ladke <rushiladke18.rsl@gmail.com>
2022-02-08 18:01:25 +0530

    :recycle: mutating data

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,27 @@ const App = () => {
   const [post, setPost] = useState(null);
 
   const { isLoading, data: posts } = useQuery('posts', () =>
-    fetcher('https://jsonplaceholder.typicode.com/posts')
+    fetcher('https://jsonplaceholder.typicode.com/posts'),
+      /**
+     * @AdditionalFeature select
+     * @objective can transform data after fetch
+     * @syntax  , {
+     *  select : result => result.splice(0,6)
+     *  }
+     */
   );
 
   if (isLoading) return <h1>Loading...</h1>;
+
+  const mutateTitle = (id : any) => {
+    queryClient.setQueryData(['post', id], (oldData : any) => {
+      if (oldData) return {
+          ...oldData,
+          title: 'Hey Title just mutated'
+        }
+      }
+    )
+  }
 
   return (
     <div className='App'>
@@ -20,16 +37,17 @@ const App = () => {
         posts &&
         posts.length > 0 &&
         posts.map((post: any) => {
-          const cachedPost = queryClient.getQueryData(['post', post.id]);
+          const cachedPost : any = queryClient.getQueryData(['post', post.id]);
           /**
            * @AdditionalInfo cachedPost
            * @objective to return true/false if the post is present is cache
            */
           return (
-            <div className='padding-class' key={post.id}>
+            <div className='blog-title' key={post.id}>
               <div onClick={() => setPost(post.id)} className='pointer'>
-                {cachedPost ? <b>"Visited"</b> : null} {post.id} - {post.title}
+                {cachedPost ? <b>"Visited"</b> : null} {post.id} - {post.title}{"  "}
               </div>
+              <button onClick={() => mutateTitle(post.id)} disabled={!cachedPost}>Mutate the inner post title after Visited</button>
             </div>
           );
         })
